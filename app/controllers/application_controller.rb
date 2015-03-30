bf9151f17ad06ba2edec7cec1bf6c32314267d1f
Nick Donias <pair+alycit.mweitzel.quackingduck@devbootcamp.com>
2015-03-30 17:16:59 -0400

    styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,10 @@ class ApplicationController < ActionController::Base
     now = Time.now.utc.to_date
     now.year - dob.year - ((now.month > dob.month || \
     (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
-  end  
+  end
 
-  # def logged_in?
-  #   !!current_user
-  # end
+  def logged_in?
+    !!current_user
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324131335) do
+ActiveRecord::Schema.define(version: 20150330194915) do
 
   create_table "photos", force: :cascade do |t|
     t.integer  "profile_id"
