ca588cc80d068a11dee634e70172ec2ca1f15354
Nick Donias <pair+alycit.mweitzel.quackingduck@devbootcamp.com>
2015-04-16 22:56:50 -0400

    errors

--- db/schema.rb
+++ db/schema.rb
@@ -13,19 +13,22 @@
 
 ActiveRecord::Schema.define(version: 20150331033500) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "message_headers", force: :cascade do |t|
-    t.string   "user_id"
-    t.string   "other_id"
-    t.string   "message_id"
+    t.text     "user_id"
+    t.text     "other_id"
+    t.text     "message_id"
     t.boolean  "is_sent"
     t.boolean  "is_read",    default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "message_headers", ["message_id"], name: "index_message_headers_on_message_id"
-  add_index "message_headers", ["other_id"], name: "index_message_headers_on_other_id"
-  add_index "message_headers", ["user_id"], name: "index_message_headers_on_user_id"
+  add_index "message_headers", ["message_id"], name: "index_message_headers_on_message_id", using: :btree
+  add_index "message_headers", ["other_id"], name: "index_message_headers_on_other_id", using: :btree
+  add_index "message_headers", ["user_id"], name: "index_message_headers_on_user_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
     t.text     "body"
@@ -34,9 +37,9 @@ ActiveRecord::Schema.define(version: 20150331033500) do
   end
 
   create_table "notifications", force: :cascade do |t|
-    t.string   "receiver_id"
-    t.string   "trigger_id"
-    t.string   "status_type"
+    t.text     "receiver_id"
+    t.text     "trigger_id"
+    t.text     "status_type"
     t.boolean  "is_read"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
@@ -44,8 +47,8 @@ ActiveRecord::Schema.define(version: 20150331033500) do
 
   create_table "photos", force: :cascade do |t|
     t.integer  "profile_id"
-    t.string   "file_file_name"
-    t.string   "file_content_type"
+    t.text     "file_file_name"
+    t.text     "file_content_type"
     t.integer  "file_file_size"
     t.datetime "file_updated_at"
     t.datetime "created_at",        null: false
@@ -53,9 +56,9 @@ ActiveRecord::Schema.define(version: 20150331033500) do
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.string   "user_id"
-    t.string   "sex"
-    t.string   "sexual_orientation"
+    t.text     "user_id"
+    t.text     "sex"
+    t.text     "sexual_orientation"
     t.text     "bio"
     t.boolean  "weights"
     t.boolean  "running"
@@ -70,16 +73,16 @@ ActiveRecord::Schema.define(version: 20150331033500) do
     t.float    "latitude"
     t.float    "longitude"
     t.integer  "zip_code"
-    t.string   "city"
+    t.text     "city"
     t.date     "birthday"
   end
 
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "status_messages", force: :cascade do |t|
-    t.string   "user_id"
-    t.string   "category"
-    t.string   "body"
+    t.text     "user_id"
+    t.text     "category"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -88,22 +91,22 @@ ActiveRecord::Schema.define(version: 20150331033500) do
     t.integer  "user_id"
     t.integer  "beg_age"
     t.integer  "end_age"
-    t.string   "sex"
-    t.string   "distance"
-    t.string   "sexual_orientation"
+    t.text     "sex"
+    t.text     "distance"
+    t.text     "sexual_orientation"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "password_digest"
+    t.text     "username"
+    t.text     "email"
+    t.text     "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password"
+    t.text     "password"
   end
 
-  add_index "users", ["username"], name: "index_users_on_username"
+  add_index "users", ["username"], name: "index_users_on_username", using: :btree
 
 end
